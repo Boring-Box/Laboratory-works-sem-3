52c5d31ff9569839c4b8caec32ca78f3542742f5
Boring-Box <boringbox@yahoo.com>
2020-09-16 11:37:53 +0300

    Lab 1

---
+++ Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class A{
+    private int a = 7;
+    private int b = 4;
+
+    public int c
+    {
+        get{return a * b;}
+    }
+
+    public int d
+    {
+        get{return a - b;}
+    }
+}
+
+public class Programm
+{
+    static void Main(){
+        A Obj = new A();
+        System.Console.WriteLine(Obj.c);
+        System.Console.WriteLine(Obj.d);
+    }
+}
